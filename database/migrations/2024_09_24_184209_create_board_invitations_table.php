feb42c5ca72bf74d763b807ae6ce2154869ee55c
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-09-25 03:09:51 +0800

    feat: Create board_invitations table migration

---
+++ database/migrations/2024_09_24_184209_create_board_invitations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('board_invitations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('board_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // The user receiving the invite
+            $table->foreignId('invited_by')->constrained('users')->onDelete('cascade'); // The user sending the invite
+            $table->enum('status', ['pending', 'accepted', 'declined'])->default('pending');
+            $table->timestamps();
+        });        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('board_invitations');
+    }
+};
