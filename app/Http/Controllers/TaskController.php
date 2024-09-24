ffc6f3c21d5bfe8a7099c11721d2e0ba462942c4
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-09-25 02:07:33 +0800

    feat: Add authorization check for showing tasks by owner or collaborator

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -78,6 +78,9 @@ class TaskController extends Controller
     public function show($boardId, $taskId)
     {
         $task = Task::with('media')->findOrFail($taskId); // Load the task with its attachments
+
+        $this->authorize('isOwnerOrCollaborator', $task);
+
         $board = Board::findOrFail($boardId); // Fetch the board based on the boardId
     
         return view('boards.tasks.show', [
