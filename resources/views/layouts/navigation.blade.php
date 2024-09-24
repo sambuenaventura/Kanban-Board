0edab687556ed8ac68d7d7e845a75713644bc774
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-09-25 03:58:30 +0800

    feat: Add navigation link for managing board invitations

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,6 +26,12 @@
                         {{ __('Boards') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('boards.manageInvitations')" :active="request()->routeIs('boards.manageInvitations')">
+                        {{ __('Board Invitations') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
