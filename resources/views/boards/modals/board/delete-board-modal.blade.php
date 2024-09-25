03a397d19c072c8eb17ad84d64f8c0b461fe1fee
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-09-25 19:06:51 +0800

    feat: Add idempotency key to delete board form

--- resources/views/boards/modals/board/delete-board-modal.blade.php
+++ resources/views/boards/modals/board/delete-board-modal.blade.php
@@ -26,6 +26,7 @@
                 <form id="deleteBoardForm" method="POST">
                     @csrf
                     @method('DELETE')
+                    <input type="hidden" name="idempotency_key" value="{{ session('idempotency_key') ?? Str::random(32) }}">                                
                     <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
                         Delete
                     </button>
