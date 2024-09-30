2a5576a96fcc8daa8671f1f20b320307aae2bf85
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-09-30 22:16:34 +0800

    refactor: Simplify migration for board_user_id in tasks table

--- database/migrations/2024_09_21_155211_replace_user_id_in_tasks_with_board_user_id.php
+++ database/migrations/2024_09_21_155211_replace_user_id_in_tasks_with_board_user_id.php
@@ -12,17 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            // Drop the user_id column and its foreign key constraint
-            if (Schema::hasColumn('tasks', 'user_id')) {
-                $table->dropForeign(['user_id']);  // Drop foreign key
-                $table->dropColumn('user_id');     // Drop user_id column
-            }
-    
             // Add the board_user_id column and reference it to the board_user table
             if (!Schema::hasColumn('tasks', 'board_user_id')) {
                 $table->foreignId('board_user_id')
-                    ->constrained('board_user') // Reference board_user table
-                    ->onDelete('cascade');      // Cascade on delete
+                    ->constrained('board_users') // Reference board_user table
+                    ->onDelete('cascade');
             }
         });
     }
@@ -37,13 +31,6 @@ return new class extends Migration
             if (Schema::hasColumn('tasks', 'board_user_id')) {
                 $table->dropForeign(['board_user_id']);
                 $table->dropColumn('board_user_id');
-            }
-    
-            // Re-add the user_id column and foreign key constraint
-            if (!Schema::hasColumn('tasks', 'user_id')) {
-                $table->foreignId('user_id')
-                    ->constrained()
-                    ->onDelete('cascade');
             }
         });
     }
