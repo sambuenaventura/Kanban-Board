0da8c03b311a40979e5f77c99ca1127f41e11f0f
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-09-30 22:46:57 +0800

    feat: Add dependency injection for Task model in TaskService

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -2,10 +2,19 @@
 
 namespace App\Services;
 
+use App\Models\Task;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class TaskService
 {
+    protected $taskModel;
+
+    public function __construct(Task $taskModel)
+    {
+        $this->taskModel = $taskModel;
+    }
+
 
 
 }
