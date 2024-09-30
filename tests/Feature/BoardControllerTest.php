d07ab0da3a66ad15051d7d27549f5f50f4a5f62a
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-10-01 03:11:55 +0800

    test: Initialize BoardService in BoardControllerTest setup method

--- tests/Feature/BoardControllerTest.php
+++ tests/Feature/BoardControllerTest.php
@@ -4,9 +4,38 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Board;
+use App\Models\BoardInvitation;
+use App\Models\BoardUser;
+use App\Models\Task;
+use App\Services\BoardService;
 
 class BoardControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected $boardService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Create instances of the models
+        $boardModel = new Board();
+        $taskModel = new Task();
+        $boardUserModel = new BoardUser();
+        $userModel = new User();
+        $boardInvitationModel = new BoardInvitation();
+
+        // Initialize the BoardService with model instances
+        $this->boardService = new BoardService(
+            $boardModel,
+            $taskModel,
+            $boardUserModel,
+            $userModel,
+            $boardInvitationModel
+        );
+    }
 
 }
