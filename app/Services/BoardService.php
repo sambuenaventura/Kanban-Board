9c2bedbcba6b62727cabb014187077400b906595
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-09-30 22:39:45 +0800

    feat: Add model properties and dependency injection to BoardService

--- app/Services/BoardService.php
+++ app/Services/BoardService.php
@@ -1,9 +1,31 @@
 <?php
 
 namespace App\Services;
+
+use App\Models\Board;
+use App\Models\BoardInvitation;
+use App\Models\BoardUser;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class BoardService
 {
+    protected $boardModel;
+    protected $taskModel;
+    protected $boardUserModel;
+    protected $userModel;
+    protected $boardInvitationModel;
+
+    // Inject the models through the constructor
+    public function __construct(Board $boardModel, Task $taskModel, BoardUser $boardUserModel, User $userModel, BoardInvitation $boardInvitationModel)
+    {
+        $this->boardModel = $boardModel;
+        $this->taskModel = $taskModel;
+        $this->boardUserModel = $boardUserModel;
+        $this->userModel = $userModel;
+        $this->boardInvitationModel = $boardInvitationModel;
+    }
+
 
 }
