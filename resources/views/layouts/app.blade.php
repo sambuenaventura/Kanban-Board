254e42e5c87450e226e7f2bc9619e03131f51a35
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-10-02 16:11:02 +0800

    chore: Add navigation.js in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
         @vite(['resources/css/app.css', 
                 'resources/js/app.js', 
                 'resources/js/update-time.js',
+                'resources/js/navigation.js',
                 ])
         
     </head>
