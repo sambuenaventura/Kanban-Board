00ac95008d279b571639d094d034be3708cedbcd
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-10-03 22:52:48 +0800

    fix: Update logo link to point to the boards index

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,27 +5,17 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('boards.index') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
-
-                <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-                {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.index')">
-                        {{ __('Tasks') }}
-                    </x-nav-link>
-                </div> --}}
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('boards.index')" :active="request()->routeIs('boards.index')">
+                    <x-nav-link :href="route('boards.index')" :active="request()->routeIs('boards.*') && !request()->routeIs('boards.manageInvitations')">
                         {{ __('Boards') }}
                     </x-nav-link>
                 </div>
+                
+                
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('boards.manageInvitations')" :active="request()->routeIs('boards.manageInvitations')" class="relative flex items-center">
