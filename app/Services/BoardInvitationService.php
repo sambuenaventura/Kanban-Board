c2c82cc48b4c3405963499df01ba52433ae9a23d
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-10-03 22:41:28 +0800

    feat: Add model properties and dependency injection to BoardInvitationService

--- app/Services/BoardInvitationService.php
+++ app/Services/BoardInvitationService.php
@@ -1,8 +1,20 @@
 <?php
 
 namespace App\Services;
+use App\Models\BoardInvitation;
+use App\Models\BoardUser;
 
 class BoardInvitationService
 {
+    protected $boardInvitationModel;
+    protected $boardUserModel;
+
+    // Inject the models through the constructor
+    public function __construct(BoardInvitation $boardInvitationModel, BoardUser $boardUserModel)
+    {
+        $this->boardInvitationModel = $boardInvitationModel;
+        $this->boardUserModel = $boardUserModel;
+    }
+
     
 }
