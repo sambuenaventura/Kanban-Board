239d7d33aa2be173c5b0309ad00bf247beb73706
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-10-04 20:12:32 +0800

    refactor: Update "Add Todo" to "Add Task" in the view

--- resources/views/boards/show.blade.php
+++ resources/views/boards/show.blade.php
@@ -35,7 +35,7 @@
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
                                 </svg>
-                                Add Todo
+                                Add Task
                             </button>
                             
                             @if ($board->user_id == Auth::id())
