d6257c52dd3488b80867427df03104303c134b68
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-10-07 19:14:19 +0800

    refactor: Optimize drag-drop.js

--- resources/views/boards/scripts/boards.drag-drop.js
+++ resources/views/boards/scripts/boards.drag-drop.js
@@ -1,14 +1,12 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    const tasks = document.querySelectorAll(".kanban-column.p-4 li");
+document.addEventListener("DOMContentLoaded", () => {
     const columns = document.querySelectorAll(
         ".flex-1.bg-gray-100.rounded-lg.overflow-hidden"
     );
 
-    tasks.forEach((task) => {
-        task.addEventListener("dragstart", dragStart);
-        task.addEventListener("dragend", dragEnd);
-    });
+    // Selectors
+    const taskSelector = ".kanban-column.p-4 li";
 
+    // Add drag event listeners to columns
     columns.forEach((column) => {
         column.addEventListener("dragover", dragOver);
         column.addEventListener("dragenter", dragEnter);
@@ -16,13 +14,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
         column.addEventListener("drop", drop);
     });
 
+    // Event delegation for tasks
+    const tasks = document.querySelectorAll(taskSelector);
+    tasks.forEach((task) => {
+        task.addEventListener("dragstart", dragStart);
+        task.addEventListener("dragend", dragEnd);
+    });
+
     function dragStart(e) {
-        e.dataTransfer.setData("text/plain", e.target.dataset.taskId);
-        this.classList.add("dragging");
+        e.dataTransfer.setData(
+            "text/plain",
+            e.target.closest(taskSelector).dataset.taskId
+        );
+        e.target.closest(taskSelector).classList.add("dragging");
     }
 
-    function dragEnd() {
-        this.classList.remove("dragging");
+    function dragEnd(e) {
+        e.target.closest(taskSelector).classList.remove("dragging");
     }
 
     function dragOver(e) {
@@ -32,71 +40,106 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     function dragEnter(e) {
         e.preventDefault();
-        const column = this.closest(
-            ".flex-1.bg-gray-100.rounded-lg.overflow-hidden"
-        );
-        if (column) {
-            column.classList.add("drag-over");
-        }
+        const column = e.currentTarget;
+        column.classList.add("drag-over");
     }
 
     function dragLeave(e) {
-        e.preventDefault();
-        const column = this.closest(
-            ".flex-1.bg-gray-100.rounded-lg.overflow-hidden"
-        );
-        if (column && !isWithinColumn(e, column)) {
-            column.classList.remove("drag-over");
-        }
-    }
-
-    function isWithinColumn(e, column) {
-        const { clientX, clientY } = e;
-        const rect = column.getBoundingClientRect();
-        return (
-            clientX >= rect.left &&
-            clientX <= rect.right &&
-            clientY >= rect.top &&
-            clientY <= rect.bottom
-        );
+        const column = e.currentTarget;
+        column.classList.remove("drag-over");
     }
 
     function drop(e) {
         e.preventDefault();
-        const column = this.closest(
-            ".flex-1.bg-gray-100.rounded-lg.overflow-hidden"
-        );
-        if (column) {
-            column.classList.remove("drag-over");
-        }
-        const taskId = e.dataTransfer.getData("text");
+        const column = e.currentTarget;
+        column.classList.remove("drag-over");
+
+        const taskId = e.dataTransfer.getData("text/plain");
         const task = document.querySelector(`[data-task-id="${taskId}"]`);
-        const targetColumn = this.querySelector(".kanban-column.p-4") || this;
+
+        // Check if the task was found
+        if (!task) {
+            console.error("Task not found with ID:", taskId);
+            return;
+        }
+
+        const targetColumn =
+            column.querySelector(".kanban-column.p-4") || column;
         const sourceColumn = task.closest(".kanban-column.p-4");
 
         if (targetColumn !== sourceColumn) {
             const taskDate = task
-                .closest(".mb-4")
-                .querySelector("h5").textContent;
+                .closest(".mb-6")
+                .querySelector("h5 span:last-child")
+                .textContent.replace("Due: ", "");
             moveTask(task, targetColumn, taskDate);
             updateTaskStatus(taskId, targetColumn.dataset.column);
 
             removeEmptyDateGroups(sourceColumn);
             sortDateGroups(targetColumn);
 
-            if (targetColumn.dataset.column === "done") {
-                task.querySelector(".text-gray-700").classList.add(
-                    "strikethrough"
-                );
-            } else {
-                task.querySelector(".text-gray-700").classList.remove(
-                    "strikethrough"
-                );
-            }
-
-            // Update task counts for both source and target columns
+            // Update task status indicators
+            updateTaskStatusIndicators(task, targetColumn.dataset.column);
             updateTaskCount(sourceColumn);
             updateTaskCount(targetColumn);
+        }
+
+        // Update strikethrough class based on the target column
+        if (targetColumn.dataset.column === "done") {
+            task.classList.add("strikethrough");
+        } else {
+            task.classList.remove("strikethrough");
+        }
+    }
+
+    function updateTaskStatusIndicators(task, newStatus) {
+        const statusBar = task.querySelector(".w-full.h-1");
+        const statusText = task.querySelector(".px-4.py-2");
+
+        if (newStatus === "done") {
+            if (statusBar) statusBar.remove();
+            if (statusText) statusText.remove();
+        } else {
+            if (!statusBar && !statusText) {
+                const placeholder = determineTaskStatus(task);
+                const newStatusBar = document.createElement("div");
+                newStatusBar.className = `w-full h-1 ${placeholder.barColor}`;
+                const newStatusText = document.createElement("div");
+                newStatusText.className = `px-4 py-2 ${placeholder.textColor} text-sm font-medium`;
+                newStatusText.textContent = placeholder.status;
+                task.appendChild(newStatusBar);
+                task.appendChild(newStatusText);
+            }
+        }
+    }
+
+    function determineTaskStatus(task) {
+        const dueDateText = task
+            .closest(".mb-6")
+            .querySelector("h5 span:last-child")
+            .textContent.replace("Due: ", "");
+        const dueDate = new Date(dueDateText);
+        const now = new Date();
+
+        if (dueDate < now) {
+            return {
+                status: "Overdue",
+                barColor: "bg-red-500",
+                textColor: "bg-red-50 text-red-700",
+            };
+        } else if (dueDate.toDateString() === now.toDateString()) {
+            return {
+                status: "Due Today",
+                barColor: "bg-yellow-500",
+                textColor: "bg-yellow-50 text-yellow-700",
+            };
+        } else if (dueDate > now && dueDate - now <= 2 * 24 * 60 * 60 * 1000) {
+            // Due within 2 days
+            return {
+                status: "Due Soon",
+                barColor: "bg-orange-500",
+                textColor: "bg-orange-50 text-orange-700",
+            };
         }
     }
 
@@ -106,7 +149,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
 
     function removeEmptyDateGroups(column) {
-        const dateGroups = column.querySelectorAll(".mb-4");
+        const dateGroups = column.querySelectorAll(".mb-6");
         dateGroups.forEach((group) => {
             if (group.querySelector("ul").children.length === 0) {
                 group.remove();
@@ -117,8 +160,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
     function findOrCreateDateGroup(column, date) {
         let dateGroup = Array.from(column.children).find(
             (child) =>
-                child.querySelector("h5") &&
-                child.querySelector("h5").textContent === date
+                child.querySelector("h5 span:last-child")?.textContent ===
+                `Due: ${date}`
         );
 
         if (!dateGroup) {
@@ -131,10 +174,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     function createDateGroup(date) {
         const div = document.createElement("div");
-        div.className = "mb-4";
+        div.className = "mb-6";
         div.innerHTML = `
-            <h5 class="text-sm font-semibold text-gray-600 mb-2">${date}</h5>
-            <ul class="space-y-3"></ul>
+            <h5 class="flex items-center text-sm font-semibold text-gray-600 mb-3">
+                <span class="material-symbols-outlined text-gray-400 text-lg mr-1">schedule</span>
+                <span class="text-gray-600">Due: ${date}</span>
+            </h5>
+            <ul class="space-y-4"></ul>
         `;
         return div;
     }
@@ -148,19 +194,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
                     .querySelector('meta[name="csrf-token"]')
                     .getAttribute("content"),
             },
-            body: JSON.stringify({
-                progress: newStatus, // Update this line to use 'progress'
-            }),
+            body: JSON.stringify({ progress: newStatus }),
         })
             .then((response) => {
-                if (!response.ok) {
+                if (!response.ok)
                     throw new Error("Network response was not ok");
-                }
                 return response.json();
             })
-            .then((data) => {
-                console.log("Task updated successfully:", data);
-            })
+            .then((data) => console.log("Task updated successfully:", data))
             .catch((error) => {
                 console.error("Error:", error);
                 alert("Error 403: This action is unauthorized.");
@@ -168,10 +209,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
 
     function sortDateGroups(column) {
-        const dateGroups = Array.from(column.querySelectorAll(".mb-4"));
+        const dateGroups = Array.from(column.querySelectorAll(".mb-6"));
         dateGroups.sort((a, b) => {
-            const dateA = new Date(a.querySelector("h5").textContent);
-            const dateB = new Date(b.querySelector("h5").textContent);
+            const dateA = new Date(
+                a
+                    .querySelector("h5 span:last-child")
+                    .textContent.replace("Due: ", "")
+            );
+            const dateB = new Date(
+                b
+                    .querySelector("h5 span:last-child")
+                    .textContent.replace("Due: ", "")
+            );
             return dateA - dateB;
         });
 
@@ -189,5 +238,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
 
     // Initial task count update
-    columns.forEach((column) => updateTaskCount(column));
+    columns.forEach(updateTaskCount);
 });
