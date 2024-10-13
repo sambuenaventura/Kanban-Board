748590ef0a45995536ce742a7448d9a75e4bf5aa
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-10-13 19:45:31 +0800

    chore: Replace header text with date and time placeholders

--- resources/views/boards/manage-invitations.blade.php
+++ resources/views/boards/manage-invitations.blade.php
@@ -1,8 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Manage Board Invitations') }}
-        </h2>
+        <div class="flex justify-between items-center">
+            <div>
+                <p id="datePlaceholder" class="text-gray-500"></p>
+                <h1 id="timePlaceholder" class="text-2xl font-bold text-gray-700"></h1>
+            </div>
+
+        </div>
     </x-slot>
     
     <div class="py-12">
